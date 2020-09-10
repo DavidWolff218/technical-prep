63d3f8e22ec7ef63e7e91cdc38f5e0f9c13569a2
doodleman218 <dcwolff218@gmail.com>
2020-09-10 12:29:57 -0500

    git set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    bold: ""
+  };
+
+  handleClick = (e) => {
+    this.setState({
+      bold: e
+    })
+  };
+
+  
+  render() {
+    return (
+      <>
+        <button id="elementA" style={{fontWeight: `${this.state.bold === "A" ? "bold" : ""}`}} onClick={() => this.handleClick("A")}>
+          Element A
+        </button>
+        <br></br>
+        <button id={"elementB"} style={{fontWeight: `${this.state.bold === "B" ? "bold" : ""}`}} onClick={() => this.handleClick("B")}>
+          Element B
+        </button>
+      </>
+    );
+  }
 }
 
 export default App;
+
+
+// import React, { Component } from "react";
+
+// export class App extends Component {
+//   state = {
+//     A: "",
+//     B: "",
+//   };
+
+//   handleClickA = () => {
+//     this.setState({
+//       A: "bold",
+//       B: ""
+//     })
+//   };
+
+//   handleClickB = () => {
+//     this.setState({
+//       A: "",
+//       B: "bold"
+//     })
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <button style={{fontWeight: `${this.state.A}`}} onClick={this.handleClickA}>
+//           Element A
+//         </button>
+//         <br></br>
+//         <button style={{fontWeight: `${this.state.B}`}} onClick={this.handleClickB}>
+//           Element B
+//         </button>
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
