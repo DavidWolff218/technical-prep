572ba78424ddca12383110ca73423fae99ad6de5
doodleman218 <dcwolff218@gmail.com>
2020-09-11 12:05:59 -0500

    setting up tech review code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,78 @@
 import React, { Component } from "react";
 
 export class App extends Component {
-  state = {
-    bold: ""
-  };
+  sampleOrders = [
+    {
+      tableNumber: 1,
+      items: [
+        {
+          itemName: "coffee",
+          quantity: 2,
+        },
+        {
+          itemName: "donuts",
+          quantity: 6,
+        },
+      ],
+    },
+    {
+      tableNumber: 2,
+      items: [
+        {
+          itemName: "bagel",
+          quantity: 1,
+        },
+      ],
+    },
+  ];
 
-  handleClick = (e) => {
-    this.setState({
-      bold: e
-    })
-  };
-
-  
-  render() {
+  Orders = () => {
     return (
       <>
-        <button id="elementA" style={{fontWeight: `${this.state.bold === "A" ? "bold" : ""}`}} onClick={() => this.handleClick("A")}>
-          Element A
-        </button>
-        <br></br>
-        <button id={"elementB"} style={{fontWeight: `${this.state.bold === "B" ? "bold" : ""}`}} onClick={() => this.handleClick("B")}>
-          Element B
-        </button>
+        <div>Table 1 - 8 items</div>
+        <div>Table 2 - 1 item</div>
       </>
     );
+  };
+
+  render() {
+    return <div>{this.Orders()}</div>;
   }
 }
-
 export default App;
 
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<Orders data={sampleOrders} />, rootElement);
+
+// import React, { Component } from "react";
+
+// export class App extends Component {
+//   state = {
+//     bold: ""
+//   };
+
+//   handleClick = (e) => {
+//     this.setState({
+//       bold: e
+//     })
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <button id="elementA" style={{fontWeight: `${this.state.bold === "A" ? "bold" : ""}`}} onClick={() => this.handleClick("A")}>
+//           Element A
+//         </button>
+//         <br></br>
+//         <button id={"elementB"} style={{fontWeight: `${this.state.bold === "B" ? "bold" : ""}`}} onClick={() => this.handleClick("B")}>
+//           Element B
+//         </button>
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
 
 // import React, { Component } from "react";
 
